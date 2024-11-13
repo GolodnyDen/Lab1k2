7c41e817c9320ee0570cdfc6c94b41731c810be9
DeUnru <hungryden15@yandex.ru>
2024-11-14 01:03:24 +0300

    [+] changes

--- Lab5/WordsStartingWithLetter.java
+++ Lab5/WordsStartingWithLetter.java
@@ -3,8 +3,8 @@ import java.util.regex.Pattern;
 
 public class WordsStartingWithLetter {
     public static void main(String[] args) {
-        String text = "You will not pass!!!";
-        char startingLetter = 'y';
+        String text = "You will not pass! There is no way to go!";
+        char startingLetter = 'w';
         String regex = "\\b" + startingLetter + "\\w*";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(text);
